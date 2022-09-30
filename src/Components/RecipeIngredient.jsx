c9917fc12eef372f3c1743500d7f09d53bc592ab
brodrigues0ll <bernardorg99@hotmail.com>
2022-09-30 19:50:21 -0300

    Req 25

--- src/Components/RecipeIngredient.jsx
+++ src/Components/RecipeIngredient.jsx
@@ -37,7 +37,7 @@ function RecipeIngredient() {
     });
   };
   RemoveLength0();
-
+  // ${recipe.strMeasure}
   return (
     <div>
       {
@@ -46,141 +46,124 @@ function RecipeIngredient() {
             <li
               data-testid="0-ingredient-name-and-measure"
             >
-              {recipe.strIngredient1}
+              {`${recipe.strMeasure1} ${recipe.strIngredient1}`}
 
             </li>
 
             <li
               data-testid="1-ingredient-name-and-measure"
             >
-              {recipe.strIngredient2}
+              {`${recipe.strMeasure2} ${recipe.strIngredient2}`}
 
             </li>
 
             <li
               data-testid="2-ingredient-name-and-measure"
             >
-              {recipe.strIngredient3}
+              {`${recipe.strMeasure3} ${recipe.strIngredient3}`}
 
             </li>
 
             <li
               data-testid="3-ingredient-name-and-measure"
             >
-              {recipe.strIngredient4}
-
+              {`${recipe.strMeasure4} ${recipe.strIngredient4}`}
             </li>
 
             <li
               data-testid="4-ingredient-name-and-measure"
             >
-              {recipe.strIngredient5}
-
+              {`${recipe.strMeasure5} ${recipe.strIngredient5}`}
             </li>
 
             <li
               data-testid="5-ingredient-name-and-measure"
             >
-              {recipe.strIngredient6}
-
+              {`${recipe.strMeasure6} ${recipe.strIngredient6}`}
             </li>
 
             <li
               data-testid="6-ingredient-name-and-measure"
             >
-              {recipe.strIngredient7}
-
+              {`${recipe.strMeasure7} ${recipe.strIngredient7}`}
             </li>
 
             <li
               data-testid="7-ingredient-name-and-measure"
             >
-              {recipe.strIngredient8}
-
+              {`${recipe.strMeasure8} ${recipe.strIngredient8}`}
             </li>
 
             <li
               data-testid="8-ingredient-name-and-measure"
             >
-              {recipe.strIngredient9}
-
+              {`${recipe.strMeasure9} ${recipe.strIngredient9}`}
             </li>
 
             <li
               data-testid="9-ingredient-name-and-measure"
             >
-              {recipe.strIngredient10}
-
+              {`${recipe.strMeasure10} ${recipe.strIngredient10}`}
             </li>
 
             <li
               data-testid="10-ingredient-name-and-measure"
             >
-              {recipe.strIngredient11}
-
+              {`${recipe.strMeasure11} ${recipe.strIngredient11}`}
             </li>
 
             <li
               data-testid="11-ingredient-name-and-measure"
             >
-              {recipe.strIngredient12}
-
+              {`${recipe.strMeasure12} ${recipe.strIngredient12}`}
             </li>
 
             <li
               data-testid="12-ingredient-name-and-measure"
             >
-              {recipe.strIngredient13}
-
+              {`${recipe.strMeasure13} ${recipe.strIngredient13}`}
             </li>
 
             <li
               data-testid="13-ingredient-name-and-measure"
             >
-              {recipe.strIngredient14}
-
+              {`${recipe.strMeasure14} ${recipe.strIngredient14}`}
             </li>
 
             <li
               data-testid="14-ingredient-name-and-measure"
             >
-              {recipe.strIngredient15}
-
+              {`${recipe.strMeasure15} ${recipe.strIngredient15}`}
             </li>
 
             <li
               data-testid="15-ingredient-name-and-measure"
             >
-              {recipe.strIngredient16}
-
+              {`${recipe.strMeasure16} ${recipe.strIngredient16}`}
             </li>
 
             <li
               data-testid="16-ingredient-name-and-measure"
             >
-              {recipe.strIngredient17}
-
+              {`${recipe.strMeasure17} ${recipe.strIngredient17}`}
             </li>
 
             <li
               data-testid="17-ingredient-name-and-measure"
             >
-              {recipe.strIngredient18}
-
+              {`${recipe.strMeasure18} ${recipe.strIngredient18}`}
             </li>
 
             <li
               data-testid="18-ingredient-name-and-measure"
             >
-              {recipe.strIngredient19}
-
+              {`${recipe.strMeasure19} ${recipe.strIngredient19}`}
             </li>
 
             <li
               data-testid="19-ingredient-name-and-measure"
             >
-              {recipe.strIngredient20}
-
+              {`${recipe.strMeasure20} ${recipe.strIngredient20}`}
             </li>
           </ul>
         ))
