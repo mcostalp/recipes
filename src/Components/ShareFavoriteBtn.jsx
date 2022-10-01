3bb5331d31b6a23cec41115168319a08d7a5531d
Bernardo Rodrigues <99976254+brodrigues0ll@users.noreply.github.com>
2022-10-01 07:20:05 -0300

    Update ShareFavoriteBtn.jsx

--- src/Components/ShareFavoriteBtn.jsx
+++ src/Components/ShareFavoriteBtn.jsx
@@ -1,50 +1,18 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { useParams } from 'react-router-dom';
-import shareIcon from '../images/shareIcon.svg';
-import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import React, { useState } from 'react';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
-import RecipesContext from '../context/RecipesContext';
-import { requestDetails } from '../helpers/Services/apiRequest';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import shareIcon from '../images/shareIcon.svg';
 
 function ShareFavoriteBtn() {
   const [isVisible, setIsVisible] = useState(false);
   const [isFavorite, setIsFavorite] = useState(false);
-  const [localResp, setLocalResp] = useState({});
-  const [storage, setStorage] = useState({});
-  const { id } = useParams();
 
-  const shareFunc = () => {
-    const SECONDS = 5000;
-    navigator.clipboard.writeText(window.location.href);
+  const handleShare = () => {
+    const SECOND = 5000;
     setIsVisible(true);
-    setTimeout(() => { setIsVisible(false); }, SECONDS);
+    window.navigator.clipboard.writeText(window.location.href);
+    setTimeout(() => { setIsVisible(false); }, SECOND);
   };
-
-  const favoriteMeal = () => {
-    setStorage({
-      id: localResp.idMeal,
-      type: localResp.strTags,
-      nationality: localResp.strArea,
-      category: localResp.strCategory,
-      alcoholicOrNot: false,
-      name: localResp.strMeal,
-      image: localResp.strMealThumb,
-    });
-    localStorage.setItem('favoriteRecipes', JSON.stringify(storage));
-  };
-
-  const {
-    pageState,
-  } = useContext(RecipesContext);
-
-  useEffect(() => {
-    const fetch = async () => {
-      const response = await requestDetails(pageState, 'recipeById', id);
-      setLocalResp(response[0]);
-      favoriteMeal();
-    };
-    fetch();
-  }, []);
 
   return (
     <>
@@ -53,7 +21,7 @@ function ShareFavoriteBtn() {
         src={ shareIcon }
         type="image"
         alt="share-btn"
-        onClick={ shareFunc }
+        onClick={ handleShare }
       />
 
       <input
@@ -61,13 +29,9 @@ function ShareFavoriteBtn() {
         src={ isFavorite ? blackHeartIcon : whiteHeartIcon }
         type="image"
         alt="favorite-btn"
-        onClick={ () => {
-          favoriteMeal();
-          setIsFavorite(!isFavorite);
-        } }
+        onClick={ () => setIsFavorite(!isFavorite) }
       />
-
-      {isVisible && <p>Link copied!</p>}
+      {isVisible && <p>Link copied!</p> }
     </>
   );
 }
