1cde9c194fef9a058980faa9729a9941eb57a874
Luis Paulo Moreira da Costa <luis@luis>
2023-03-30 01:30:16 -0300

    Removed media query

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import DoneRecipes from './Pages/DoneRecipes';
-import Drinks from './Pages/Drinks';
-import FavoriteRecipes from './Pages/FavoriteRecipes';
-import Login from './Pages/Login';
-import Meals from './Pages/Meals';
-import Profile from './Pages/Profile';
-import MealsDetails from './Pages/MealsDetails';
-import DrinksDetails from './Pages/DrinksDetails';
-import RecipeInProgress from './Pages/RecipeInProgress';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import DoneRecipes from "./Pages/DoneRecipes";
+import Drinks from "./Pages/Drinks";
+import FavoriteRecipes from "./Pages/FavoriteRecipes";
+import Login from "./Pages/Login";
+import Meals from "./Pages/Meals";
+import Profile from "./Pages/Profile";
+import MealsDetails from "./Pages/MealsDetails";
+import DrinksDetails from "./Pages/DrinksDetails";
+import RecipeInProgress from "./Pages/RecipeInProgress";
+import "./App.css";
 
 function App() {
   return (
     <>
       <div className="bg-image" />
       <Switch>
-        <Route exact path="/recipe-app" component={ Login } />
-        <Route exact path="/meals" component={ Meals } />
-        <Route exact path="/meals/:id" component={ MealsDetails } />
-        <Route exact path="/meals/:id/in-progress" component={ RecipeInProgress } />
-        <Route exact path="/drinks" component={ Drinks } />
-        <Route exact path="/drinks/:id" component={ DrinksDetails } />
-        <Route exact path="/drinks/:id/in-progress" component={ RecipeInProgress } />
-        <Route exact path="/done-recipes" component={ DoneRecipes } />
-        <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
-        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/recipes" component={Login} />
+        <Route exact path="/meals" component={Meals} />
+        <Route exact path="/meals/:id" component={MealsDetails} />
+        <Route
+          exact
+          path="/meals/:id/in-progress"
+          component={RecipeInProgress}
+        />
+        <Route exact path="/drinks" component={Drinks} />
+        <Route exact path="/drinks/:id" component={DrinksDetails} />
+        <Route
+          exact
+          path="/drinks/:id/in-progress"
+          component={RecipeInProgress}
+        />
+        <Route exact path="/done-recipes" component={DoneRecipes} />
+        <Route exact path="/favorite-recipes" component={FavoriteRecipes} />
+        <Route exact path="/profile" component={Profile} />
       </Switch>
     </>
   );
